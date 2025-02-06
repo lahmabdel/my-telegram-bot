e6dea0094cf37b4e9a559036a00018a13fa7791a
lahmabdel <lahmabdel@gmail.com>
2025-02-06 02:12:51 +0100

    Initial commit for Render deployment test 1

--- sayhi.py
+++ sayhi.py
@@ -29,7 +29,7 @@ def check_button_in_page(url):
 
 
 # Function to send a message
-def send_telegram_message():
+def send_telegram_message(nada=0):
     url_to_check = "https://www.arpej.fr/fr/residence/nicolas-appert-residence-etudiante-ivry-sur-seine/"
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     if check_button_in_page(url_to_check):
@@ -46,8 +46,33 @@ def send_telegram_message():
                 logging.error("Failed to send message: %s", response.text)
         except Exception as e:
             logging.error("Error sending message: %s", str(e))
+    elif nada:
+        message = f"still running yazbi 3yit"
+    else:
+        return 0
+    
 
+    payload = {
+    "chat_id": TELEGRAM_CHAT_ID,
+    "text": message
+        }
+    try:
+        response = requests.post(url, json=payload)
+        if response.status_code == 200:
+            logging.info("Message sent successfully")
+        else:
+            logging.error("Failed to send message: %s", response.text)
+    except Exception as e:
+        logging.error("Error sending message: %s", str(e))
+
+
+start_time = time.time()
 if __name__ == "__main__":
     while True:
         send_telegram_message()
+        
+        current_time = time.time()
+        if current_time - start_time > 1800:
+            send_telegram_message(1)
+            start_time = current_time  # Reset the timer
         time.sleep(600)  # Wait 30 minutes before running again
